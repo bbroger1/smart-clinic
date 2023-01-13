a5c8c8d97d177c5c3bf174e7537cf29b25a3b26e
Edmar-Sousa <edmarsousamendes4@gmail.com>
2023-01-12 21:47:51 -0300

    create schema of table doctors

--- app/Models/doctor.php
+++ app/Models/doctor.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class doctor extends Model
+class Doctor extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'lastName', 'phoneNumber', 'cpf', 'sexo', 'state', 'uf', 'area'];
 }
--- database/migrations/2023_01_13_003508_create_doctors_table.php
+++ database/migrations/2023_01_13_003508_create_doctors_table.php
@@ -6,24 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateDoctorsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name', 20);
+            $table->string('lastName', 20);
+
+            $table->string('phoneNumber', 15);
+            $table->string('cpf', 14);
+            $table->string('sexo', 15);
+
+            $table->string('state', 20);
+            $table->char('uf', 2);
+
+            $table->string('area', 25);
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('doctors');
