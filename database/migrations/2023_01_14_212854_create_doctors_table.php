9a190e59afc76540ac7381c9b60e7965228b8972
Edmar-Sousa <edmarsousamendes4@gmail.com>
2023-01-14 18:34:19 -0300

    change order create migrations

--- database/migrations/2023_01_14_212854_create_doctors_table.php
+++ database/migrations/2023_01_14_212854_create_doctors_table.php
@@ -6,6 +6,11 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateDoctorsTable extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('doctors', function (Blueprint $table) {
@@ -17,18 +22,21 @@ class CreateDoctorsTable extends Migration
             $table->string('phoneNumber', 15);
             $table->string('cpf', 14);
 
-            $table->string('state', 20);
+            $table->string('city', 20);
             $table->char('uf', 2);
 
             $table->string('area', 25);
 
             $table->foreignId('genre')->references('id')->on('genres');
-
             $table->timestamps();
         });
     }
 
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::dropIfExists('doctors');
